2766707c4b9f09493cd9d3e4104db4ad3cfdd179
dipak2811 <gorfadd123@gmail.com>
2024-02-19 22:06:46 +0530

    Feat: pagination added in listing api for list, project and task

--- src/services/list.service.ts
+++ src/services/list.service.ts
@@ -1,9 +1,10 @@
+import { FindManyOptions } from "typeorm";
 import { List } from "../entities/list.entity";
 import { AppDataSource } from "../utils/data-source";
 
 const projectRepository = AppDataSource.getRepository(List);
 
-export const createListService= async (
+export const createListService = async (
   name: string,
   path: string,
   folderId: string
@@ -16,10 +17,39 @@ export const createListService= async (
     })
   );
 };
-export const getAllListService = async (folderId: string) => {
-  const list = await projectRepository.find({
+
+export const getAllListService = async (
+  folderId: string,
+  page: number,
+  pageSize: number,
+  sortBy: string
+): Promise<{
+  pageNo: number;
+  totalPages: number;
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
+  totalDocs: number;
+  lists: any[];
+}> => {
+  const options: FindManyOptions = {
     relations: ["folder"],
     where: { folder: { id: folderId } },
-  });
-  return list;
+    skip: (page - 1) * pageSize,
+    take: pageSize,
+    order: { [sortBy]: "DESC" },
+  };
+
+  const [lists, totalDocs] = await projectRepository.findAndCount(options);
+  const totalPages = Math.ceil(totalDocs / pageSize);
+  const hasNextPage = page < totalPages;
+  const hasPrevPage = page > 1;
+
+  return {
+    pageNo: page,
+    totalPages: totalPages,
+    hasNextPage: hasNextPage,
+    hasPrevPage: hasPrevPage,
+    totalDocs: totalDocs,
+    lists: lists,
+  };
 };
--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,37 +1,80 @@
+import { FindManyOptions } from "typeorm";
 import { Task } from "../entities/task.entity";
 import { ITask } from "../interfaces/task.interface";
 import { AppDataSource } from "../utils/data-source";
 
 const projectRepository = AppDataSource.getRepository(Task);
 
-export const create = async (reqBody:ITask) => {
-    const { name, description, status, label,  dueDate, priority, attachment, comments, list, assignees,reporterId } = reqBody;
-    
-    const newAssignees = assignees.map(assignee => ({ id: assignee.id }));
+export const create = async (reqBody: ITask) => {
+  const {
+    name,
+    description,
+    status,
+    label,
+    dueDate,
+    priority,
+    attachment,
+    comments,
+    list,
+    assignees,
+    reporterId,
+  } = reqBody;
 
-    return await projectRepository.save(
-        projectRepository.create({
-            name,
-            description,
-            status,
-            label,
-            reporterId,
-            dueDate,
-            priority,
-            attachment,
-            comments,
-            list: { id: list.id },
-            assignees: newAssignees
-        })
-    );
+  const newAssignees = assignees.map((assignee) => ({ id: assignee.id }));
+
+  return await projectRepository.save(
+    projectRepository.create({
+      name,
+      description,
+      status,
+      label,
+      reporterId,
+      dueDate,
+      priority,
+      attachment,
+      comments,
+      list: { id: list.id },
+      assignees: newAssignees,
+    })
+  );
 };
 
-
-
-export const getAllTasksService = async (listId: string) => {
-  const list = await projectRepository.find({
+export const getAllTasksService = async (
+  listId: string,
+  page: number,
+  pageSize: number,
+  sortBy: string
+): Promise<{
+  pageNo: number;
+  totalPages: number;
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
+  totalDocs: number;
+  tasks: any[];
+}> => {
+  const options: FindManyOptions = {
     relations: ["list"],
     where: { list: { id: listId } },
-  });
-  return list;
+    skip: (page - 1) * pageSize,
+    take: pageSize,
+    order: { [sortBy]: "DESC" },
+  };
+
+  const [tasks, totalTasksCount] = await projectRepository.findAndCount(
+    options
+  );
+
+  const totalDocs = totalTasksCount;
+  const totalPages = Math.ceil(totalTasksCount / pageSize);
+  const hasNextPage = page < totalPages;
+  const hasPrevPage = page > 1;
+
+  return {
+    pageNo: page,
+    totalPages: totalPages,
+    hasNextPage: hasNextPage,
+    hasPrevPage: hasPrevPage,
+    totalDocs: totalDocs,
+    tasks: tasks,
+  };
 };
